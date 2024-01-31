426a4dc59f7f8f92edfa90110b16892392a5ff7f
Paula Reyes <paulareyescerro@gmail.com>
2024-01-31 20:46:06 -0300

    arreglé error de js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,38 @@
-
 //smooth scroll
-$(document).ready(function(){
-  $("a").on('click', function(event) {
+$(document).ready(function () {
+  $("a").on("click", function (event) {
     if (this.hash !== "") {
       event.preventDefault();
       var hash = this.hash;
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-        window.location.hash = hash;
-      });
-    } 
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        800,
+        function () {
+          window.location.hash = hash;
+        }
+      );
+    }
   });
 });
 
 
+//Selector de etiqueta y método "on" con evento click para cambiar color letra footer
+$(document).ready(function () {
+  $("#titulofooter").on("click", function () {
+    $("#titulofooter").css("color", "black");
+  });
+});
+
+
+
+
+//Toggle para hacer desaparecer iconos al hacer click
+$(document).ready(function () {
+$("i").click(function () {
+$("i").toggle(1000);
+});
+});
 
 
